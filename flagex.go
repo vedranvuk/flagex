0a84f1064a3ffe5527d2035eaa450e0886a13801
vedranvuk <vedranvuk82@gmail.com>
2020-01-05 10:57:52 +0100

    first working

---
+++ flagex.go
@@ -0,0 +1,296 @@
+// Not thread-safe.
+package flagex
+
+import (
+	"strings"
+
+	"github.com/vedranvuk/errorex"
+)
+
+var (
+	// ErrFlag is the base flagex error.
+	ErrFlag = errorex.New("flagex")
+	// ErrInvalid is returned when an invalid flag key is specified.
+	ErrInvalid = ErrFlag.WrapFormat("invalid key")
+	// ErrNotFound is returned when a non existent key is requested.
+	ErrNotFound = ErrFlag.WrapFormat("key '%s' not found")
+	// ErrDupKey is returned when a flag with a duplicate key is being registered.
+	ErrDupKey = ErrFlag.WrapFormat("duplicate key '%s'")
+	// ErrDupShort is returned when a flag with a duplicate shortkey is being registered.
+	ErrDupShort = ErrFlag.WrapFormat("duplicate shortkey '%s'")
+	// ErrExcl is returned when a more than flag from an exclusive set is parsed.
+	ErrExcl = ErrFlag.WrapFormat("'%s' is exclusive to '%s'")
+	// ErrRequired is returned when a required flag was not parsed.
+	ErrRequired = ErrFlag.WrapFormat("required key '%s' not specified")
+	// ErrSwitch is returned when a switch was passed a param.
+	ErrSwitch = ErrFlag.WrapFormat("switch '%s' takes no params")
+	// ErrParams is returned when Parse is called with empty params.
+	ErrParams = ErrFlag.Wrap("parse invoken with no params")
+	// ErrSub is returned when a sub switsh was parsed with no args following it.
+	ErrSub = ErrFlag.WrapFormat("sub '%s' invoken with no params")
+)
+
+// FlagKind specifies Flag kind.
+type FlagKind byte
+
+const (
+	// KindOptional marks a flag as optional as well as its params.
+	KindOptional FlagKind = iota
+	// KindRequired marks a flag as required as well as is its params.
+	KindRequired
+	// KindSwitch marks a flag as optional that takes no params.
+	// Flags with sub flags take no flags and are marked as KindSwitch.
+	KindSwitch
+)
+
+// Flag represents a defined flag.
+type Flag struct {
+	key, shortkey, help, defval string
+
+	kind      FlagKind
+	sub       *Flags
+	excl      bool
+	parsed    bool
+	parsedval bool
+	value     string
+}
+
+// Key returns Flag key.
+func (f *Flag) Key() string { return f.key }
+
+// Shortkey returns Flag shortkey.
+func (f *Flag) Shortkey() string { return f.shortkey }
+
+// Help returns Flag help text.
+func (f *Flag) Help() string { return f.help }
+
+// Default is returned by Value if no value was parsed for this Flag.
+func (f *Flag) Default() string { return f.defval }
+
+// Excl
+func (f *Flag) Excl() bool { return f.excl }
+
+// Parsed returns if this Flag was parsed.
+func (f *Flag) Parsed() bool { return f.parsed }
+
+// ParsedVal returns if FLag value was parsed.
+func (f *Flag) ParsedVal() bool { return f.parsedval }
+
+// Kind returns Flag kind.
+func (f *Flag) Kind() FlagKind { return f.kind }
+
+// Kind returns current FLag value.
+func (f *Flag) Value() string {
+	if !f.parsed || !f.parsedval {
+		return f.defval
+	}
+	return f.value
+}
+
+// Flags defines a set of unique flags.
+type Flags struct {
+	keys  map[string]*Flag
+	short map[string]string
+}
+
+// New creates a new Flags instance.
+func New() *Flags {
+	return &Flags{
+		keys:  make(map[string]*Flag),
+		short: make(map[string]string),
+	}
+}
+
+// def defines a flag then returns it or an error.
+func (f *Flags) def(key, shortkey, help, defval string, typ FlagKind) (*Flag, error) {
+	if key == "" {
+		return nil, ErrInvalid
+	}
+	if _, ok := f.keys[key]; ok {
+		return nil, ErrDupKey.WithArgs(key)
+	}
+	if _, ok := f.short[shortkey]; shortkey != "" && ok {
+		return nil, ErrDupShort.WithArgs(shortkey)
+	}
+	flag := &Flag{key, shortkey, help, defval, typ, nil, false, false, false, ""}
+	f.keys[key] = flag
+	if shortkey != "" {
+		f.short[shortkey] = key
+	}
+	return flag, nil
+}
+
+// Def defines a flag under specified key and optional
+// longkey with specified help and default value defval.
+// key and shortkey must be unique in Flags, shortkey is optional.
+// If a non-nil error is returned flag was not defined.
+func (f *Flags) Def(key, shortkey, help, defval string, typ FlagKind) (err error) {
+	_, err = f.def(key, shortkey, help, defval, typ)
+	return
+}
+
+// Sub defines child Flags under specified key and optional shortkey which
+// must be unique in these Flags. When invoken rest of params are passed to it.
+// help defines the flag help. If a non-nil error is returned flag was not defined.
+func (f *Flags) Sub(key, shortkey, help string, sub *Flags) error {
+	flag, err := f.def(key, shortkey, help, "", KindSwitch)
+	if err != nil {
+		return err
+	}
+	flag.sub = sub
+	return nil
+}
+
+// Key returns Flag if under specified key and a truth if it exists.
+func (f *Flags) Key(key string) (flag *Flag, truth bool) {
+	flag, truth = f.keys[key]
+	return
+}
+
+// Short returns Flag under specified shortkey and a truth if it exists.
+func (f *Flags) Short(shortkey string) (flag *Flag, truth bool) {
+	return f.Key(f.short[shortkey])
+}
+
+// Exclusive sets specified keys as mutually exclusive in Flags.
+// If more than one key from exclusive group are parsed, parse will error.
+// Keys must already be defined.
+// Subsequent calls redefine exclusivity.
+func (f *Flags) Exclusive(keys ...string) error {
+	for _, flag := range f.keys {
+		flag.excl = false
+	}
+	for _, key := range keys {
+		flag, ok := f.Key(key)
+		if !ok {
+			return ErrNotFound.WithArgs(key)
+		}
+		flag.excl = true
+	}
+	return nil
+}
+
+// findflag finds a flag by key or shortkey from arg and
+// returns it if found and truth if exists.
+func (f *Flags) findflag(arg string) (*Flag, bool) {
+	flag, ok := f.Short(strings.TrimPrefix(arg, "-"))
+	if !ok {
+		flag, ok = f.Key(strings.TrimPrefix(arg, "--"))
+	}
+	if !ok {
+		return nil, false
+	}
+	return flag, true
+}
+
+// isexcl checks if the flag is in exclusive list.
+func (f *Flags) isexcl(flag *Flag) bool {
+	return f.keys[flag.Key()].Excl()
+}
+
+// consume marks a flag as parsed and sets its value if not empty.
+func (f *Flags) consume(key, value string) error {
+
+	flag, ok := f.keys[key]
+	if !ok {
+		return ErrNotFound.WithArgs(key)
+	}
+	if flag.Parsed() {
+		return ErrDupKey.WithArgs(key)
+	}
+	if flag.Excl() {
+		for _, v := range f.keys {
+			if v.Parsed() && v.Excl() {
+				return ErrExcl.WithArgs(v.Key(), key)
+			}
+		}
+	}
+	flag.parsed = true
+	if value != "" {
+		flag.value = value
+		flag.parsedval = true
+	}
+	return nil
+}
+
+// Parse parses specified args.
+func (f *Flags) Parse(args []string) error {
+	var flag *Flag
+	for _, flag = range f.keys {
+		flag.parsed = false
+		flag.parsedval = false
+		flag.value = ""
+	}
+	var ok bool
+	var saved string
+	var arg string
+	for i := 0; i < len(args); i++ {
+		arg = strings.TrimSpace(args[i])
+		if arg == "" {
+			continue
+		}
+		flag, ok = f.findflag(arg)
+
+		if !ok {
+			if saved == "" {
+				saved = arg
+				continue
+			}
+			flag, ok = f.findflag(saved)
+			if !ok {
+				return ErrNotFound.WithArgs(saved)
+			}
+			if flag.Kind() == KindSwitch {
+				return ErrSwitch.WithArgs(flag.Key())
+			}
+			f.consume(flag.Key(), arg)
+			saved = ""
+			continue
+		}
+
+		if saved == "" {
+			if flag.sub != nil {
+				if i == len(args)-1 {
+					return ErrSub.WithArgs(flag.Key())
+				}
+				return flag.sub.Parse(args[i+1:])
+			}
+			saved = args[i]
+			continue
+		}
+
+		flag, ok = f.findflag(saved)
+		if !ok {
+			return ErrNotFound.WithArgs(saved)
+		}
+		if flag.Kind() == KindRequired {
+			return ErrRequired.WithArgs(saved)
+		}
+		if err := f.consume(flag.Key(), ""); err != nil {
+			return err
+		}
+		saved = arg
+	}
+	if saved != "" {
+		flag, ok := f.findflag(saved)
+		if !ok {
+			return ErrNotFound.WithArgs(saved)
+		}
+		if err := f.consume(flag.Key(), ""); err != nil {
+			return err
+		}
+	}
+	noparse := true
+	for _, flag = range f.keys {
+		if flag.Kind() == KindRequired && !flag.Parsed() {
+			return ErrRequired.WithArgs(flag.Key())
+		}
+		if flag.Parsed() {
+			noparse = false
+		}
+	}
+	if noparse {
+		return ErrParams
+	}
+	return nil
+}
---
+++ flagex_test.go
@@ -0,0 +1,338 @@
+package flagex
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+	"testing"
+)
+
+func TestFlags(t *testing.T) {
+
+	var (
+		Args = []string{
+			"--config",
+			"config.json",
+			"--ip",
+			"10.0.0.1",
+			"-v",
+			"-M",
+		}
+	)
+
+	f := New()
+	if err := f.Def("username", "u", "specify username", "guest", KindOptional); err != nil {
+		t.Fatalf("Def '%s' failed ", "username")
+	}
+	if err := f.Def("ip", "", "specify ip address", "127.0.0.1", KindRequired); err != nil {
+		t.Fatalf("Def '%s' failed ", "ip")
+	}
+	if err := f.Def("config", "c", "specify config file", "", KindRequired); err != nil {
+		t.Fatalf("Def '%s' failed ", "config")
+	}
+	if err := f.Def("verbose", "v", "v for verbose", "", KindSwitch); err != nil {
+		t.Fatalf("Def '%s' failed ", "verbose")
+	}
+	if err := f.Def("mode", "M", "use mode", "best", KindOptional); err != nil {
+		t.Fatalf("Def '%s' failed ", "version")
+	}
+
+	if err := f.Parse(Args); err != nil {
+		t.Fatal(err)
+	}
+
+	flag, ok := f.Key("username")
+	if !ok {
+		t.Fatal()
+	}
+	if flag.Parsed() {
+		t.Fatal()
+	}
+	if flag.Value() != "guest" {
+		t.Fatal()
+	}
+
+	if _, ok = f.Short("u"); !ok {
+		t.Fatal()
+	}
+
+	flag, ok = f.Key("ip")
+	if !ok {
+		t.Fatal()
+	}
+	if !flag.Parsed() {
+		t.Fatal()
+	}
+	if flag.Value() != "10.0.0.1" {
+		t.Fatal()
+	}
+
+	flag, ok = f.Key("config")
+	if !ok {
+		t.Fatal()
+	}
+	if !flag.Parsed() {
+		t.Fatal()
+	}
+	if flag.Value() != "config.json" {
+		t.Fatal()
+	}
+
+	if _, ok = f.Short("c"); !ok {
+		t.Fatal()
+	}
+
+	flag, ok = f.Key("verbose")
+	if !ok {
+		t.Fatal()
+	}
+	if !flag.Parsed() {
+		t.Fatal()
+	}
+	if v := flag.Value(); v != "" {
+		t.Fatal(v)
+	}
+
+	if _, ok = f.Short("v"); !ok {
+		t.Fatal()
+	}
+
+	flag, ok = f.Key("mode")
+	if !ok {
+		t.Fatal()
+	}
+	if !flag.Parsed() {
+		t.Fatal()
+	}
+	if v := flag.Value(); v != "best" {
+		t.Fatal(v)
+	}
+
+	if _, ok = f.Short("M"); !ok {
+		t.Fatal("h")
+	}
+}
+
+func TestMux(t *testing.T) {
+
+	type FlagItem struct {
+		Key      string
+		ShortKey string
+		Help     string
+		Default  string
+		Kind     FlagKind
+		Sub      *Flags
+	}
+
+	var PackageItems = []FlagItem{
+		FlagItem{
+			"list",
+			"l",
+			"list packages",
+			"",
+			KindOptional,
+			nil,
+		},
+		FlagItem{
+			"export",
+			"e",
+			"export package list",
+			"",
+			KindOptional,
+			nil,
+		},
+		FlagItem{
+			"csv",
+			"c",
+			"use csv format",
+			"",
+			KindOptional,
+			nil,
+		},
+	}
+	var PackageExcl = []string{
+		"list",
+		"export",
+	}
+
+	var DatabaseItems = []FlagItem{
+		FlagItem{
+			"clean",
+			"c",
+			"clean database",
+			"",
+			KindOptional,
+
+			nil,
+		},
+		FlagItem{
+			"backup",
+			"b",
+			"backup database",
+			"",
+			KindOptional,
+
+			nil,
+		},
+	}
+	var DatabaseExcl = []string{
+		"clean",
+		"backup",
+	}
+
+	var SyncItems = []FlagItem{
+		FlagItem{
+			"install",
+			"i",
+			"install a package",
+			"",
+			KindOptional,
+
+			nil,
+		},
+		FlagItem{
+			"uninstall",
+			"u",
+			"uninstall a package",
+			"",
+			KindOptional,
+
+			nil,
+		},
+		FlagItem{
+			"update",
+			"b",
+			"update packages",
+			"",
+			KindOptional,
+			nil,
+		},
+		FlagItem{
+			"target",
+			"t",
+			"sync target (required)",
+			"",
+			KindRequired,
+			nil,
+		},
+		FlagItem{
+			"mode",
+			"m",
+			"sync mode",
+			"best",
+			KindRequired,
+			nil,
+		},
+		FlagItem{
+			"verbose",
+			"v",
+			"verbose output",
+			"",
+			KindSwitch,
+			nil,
+		},
+	}
+
+	var SyncExcl = []string{
+		"install",
+		"uninstall",
+		"update",
+	}
+
+	pkg := New()
+	for _, fi := range PackageItems {
+		pkg.Def(fi.Key, fi.ShortKey, fi.Help, fi.Default, fi.Kind)
+	}
+	pkg.Exclusive(PackageExcl[:]...)
+
+	dbs := New()
+	for _, fi := range DatabaseItems {
+		dbs.Def(fi.Key, fi.ShortKey, fi.Help, fi.Default, fi.Kind)
+	}
+	dbs.Exclusive(DatabaseExcl[:]...)
+
+	snc := New()
+	for _, fi := range SyncItems {
+		snc.Def(fi.Key, fi.ShortKey, fi.Help, fi.Default, fi.Kind)
+	}
+	snc.Exclusive(SyncExcl[:]...)
+
+	var RootItems = []FlagItem{
+		FlagItem{
+			"packages",
+			"P",
+			"work with packages",
+			"",
+			KindOptional,
+
+			pkg,
+		},
+		FlagItem{
+			"database",
+			"D",
+			"work with database",
+			"",
+			KindOptional,
+
+			dbs,
+		},
+		FlagItem{
+			"sync",
+			"S",
+			"package sync",
+			"",
+			KindOptional,
+
+			snc,
+		},
+	}
+
+	flag := New()
+	for _, fi := range RootItems {
+		flag.Sub(fi.Key, fi.ShortKey, fi.Help, fi.Sub)
+	}
+
+	type TestItem struct {
+		Args        string
+		ExpectedErr error
+	}
+
+	var TestItems = []TestItem{
+		/*
+		 */
+		TestItem{"", ErrParams},
+		TestItem{"-P -l", nil},
+		TestItem{"-P -e", nil},
+		TestItem{"-P -l -c", nil},
+		TestItem{"-P -e -c", nil},
+		TestItem{"-P -l -e", ErrExcl},
+		TestItem{"-D -c", nil},
+		TestItem{"-D -b", nil},
+		TestItem{"-D -c -b", ErrExcl},
+		TestItem{"-S -i", ErrRequired},
+		TestItem{"-S -t -i", ErrRequired},
+		TestItem{"-S -t home -m improved -u", nil},
+		TestItem{"-S -t home -m new -i", nil},
+		TestItem{"-S -t home -b", ErrRequired},
+		TestItem{"-S -t home -i -u", ErrExcl},
+		TestItem{"-S -t home -i -b", ErrExcl},
+		TestItem{"-S -t home -u -b", ErrExcl},
+		TestItem{"-S -t home -i -v extra", ErrSwitch},
+		TestItem{"-S -i -v -t", ErrRequired},
+		TestItem{"-S -? -v", ErrNotFound},
+		TestItem{"-S -v -?", ErrSwitch},
+		TestItem{"-S -? -!", ErrNotFound},
+		TestItem{"-S", ErrSub},
+		TestItem{"-?", ErrNotFound},
+		TestItem{"-S -t home -m new -v -v", ErrDupKey},
+	}
+
+	for i := 0; i < len(TestItems); i++ {
+		err := flag.Parse(strings.Split(TestItems[i].Args, " "))
+		fmt.Printf("'%s': expected '%v', got '%v'\n", TestItems[i].Args, TestItems[i].ExpectedErr, err)
+		if !errors.Is(err, TestItems[i].ExpectedErr) {
+			log.Fatalf("'%s': expected '%v', got '%v'", TestItems[i].Args, TestItems[i].ExpectedErr, err)
+		}
+	}
+}
